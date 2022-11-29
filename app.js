857fedf353b4f93510ecca458b179193529b9f3d
Jovan-Nsty <jova.zyo@gmail.com>
2022-11-29 15:35:17 +0100

    Made an overall design for the program, custom grid of divs with proper dom manipulation/flexbox functionality

---
+++ app.js
@@ -0,0 +1,30 @@
+const numberOfDivs = 10;
+const container = document.getElementById('container');
+
+// draw divs in a div container
+function draw(numberOfDivs) {
+    for (let i = 0; i < numberOfDivs; i++) {
+        const divColumn = document.createElement('div');
+        divColumn.classList.add('div-column');
+
+        for (let j = 0; j < numberOfDivs; j++) {
+            const divElement = document.createElement('div');
+            divElement.style.border = '1px solid black';
+            divElement.classList.add('div-element');
+
+            divColumn.appendChild(divElement);
+        }
+
+        container.appendChild(divColumn);
+    }
+}
+
+draw(numberOfDivs);
+
+const divSelection = document.querySelectorAll('.div-element');
+
+divSelection.forEach(function (n) {
+    n.addEventListener('mouseover', () => {
+        n.style.backgroundColor = 'red';
+    })
+})
