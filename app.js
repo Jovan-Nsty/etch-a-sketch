3016703596dd50aa3ce88d2c5d031625a665ccab
Jovan-Nsty <jova.zyo@gmail.com>
2022-11-30 12:52:19 +0100

    Added grid reset functionality

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 let numberOfDivs = 16;
+let divColumn;
+let divElement;
 const container = document.getElementById('container');
+const resetBtn = document.getElementById('reset');
+
+// grid reset
+resetBtn.addEventListener('click', () => {
+    gridDelete();
+    draw(numberOfDivs);
+    paint();
+})
 
 // draw divs in a div container
 function draw(numberOfDivs) {
     for (let i = 0; i < numberOfDivs; i++) {
-        const divColumn = document.createElement('div');
+        divColumn = document.createElement('div');
         divColumn.classList.add('div-column');
 
         for (let j = 0; j < numberOfDivs; j++) {
-            const divElement = document.createElement('div');
+            divElement = document.createElement('div');
             divElement.style.border = '1px solid black';
             divElement.classList.add('div-element');
 
@@ -19,12 +29,28 @@ function draw(numberOfDivs) {
     }
 }
 
-draw(numberOfDivs);
+// activate canvas
+function paint() {
+    const divSelection = document.querySelectorAll('.div-element');
 
-const divSelection = document.querySelectorAll('.div-element');
-
-divSelection.forEach(function (n) {
-    n.addEventListener('mouseover', () => {
-        n.style.backgroundColor = 'red';
+    divSelection.forEach(function (n) {
+        n.addEventListener('mouseover', () => {
+            n.style.backgroundColor = 'red';
+        })
     })
-})
+}
+
+// delete function
+function gridDelete() {
+    const columns = document.querySelectorAll('.div-column');
+    columns.forEach(column => {
+        const elements = document.querySelectorAll('.div-element')
+        elements.forEach(element => {
+            element.remove();
+        })
+        column.remove();
+    })
+}
+
+draw(numberOfDivs);
+paint();
